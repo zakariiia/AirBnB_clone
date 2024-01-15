ef31e22a6ffcaaf9dc6d7c0c89a57506b585e34d
zakariiia <136108495+zakariiia@users.noreply.github.com>
2024-01-15 16:15:24 +0100

    Update style state.py

--- models/state.py
+++ models/state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-Module for the State class.
-"""
+"""Module for the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    Represent a state.
-        name (str): The name of the state.
+    """Represent a state.
+
+    Attributes:
+        name (str): The state name.
     """
 
     name = ""
