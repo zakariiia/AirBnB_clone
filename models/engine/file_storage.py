f10149a30598e7b8ed3179816f52688e494625a2
zakariiia <136108495+zakariiia@users.noreply.github.com>
2024-01-15 17:13:15 +0100

    Update style for file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define the FileStorage class."""
 import json
 import os
 from models.base_model import BaseModel
@@ -10,40 +11,28 @@ from models.amenity import Amenity
 from models.place import Place
 
 
-
 class FileStorage:
+    """FileStorage class to store data."""
 
-    """
-    FileStorage class to store the data
-    """
     __file_path = "file.json"
 
     __objects = {}
 
     def all(self):
-        """
-        returns the dictionary __objects
-        """
+        """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        sets in __objects the obj with key <obj class name>.id
-        """
+        """Set in __objects the obj with key <obj class name>.id."""
         obj_c_nme = obj.__class__.__name__
-
-        key = f"{obj_c_nme}.{obj.id}" # class name.idù is the key, value is the data (object)
-
+        # class name.id is the key, value is the data (object)
+        key = f"{obj_c_nme}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file (path: __file_path)
-        """
+        """Serialize __objects to the JSON file (path: __file_path)."""
         obj_dict = {}
-
         all_objs = FileStorage.__objects
-
 
         for obj in all_objs.keys():
             obj_dict[obj] = all_objs[obj].to_dict()
@@ -52,12 +41,15 @@ class FileStorage:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+        """Deserialize the JSON file.
+
+        Deserialize the JSON file to __objects (only if the
+        JSON file (__file_path) exists;
+        otherwise, do nothing.
         If the file doesn’t exist, no exception should be raised)
         """
         file_s = FileStorage.__file_path
-        if os.path.isfile(file_s): # check If the file exist 
+        if os.path.isfile(file_s):  # check If the file exist
             with open(file_s, "r", encoding="utf-8") as f:
                 try:
                     obj_dict = json.load(f)
@@ -75,4 +67,4 @@ class FileStorage:
                         # print(FileStorage.__objects)
                 except Exception:
                     print("can't reload the data")
-                    pass 
+                    pass
