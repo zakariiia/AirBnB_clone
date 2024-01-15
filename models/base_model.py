ff9cba0472f1ef49b93154a19cbcc4a953c407f5
zakariiia <136108495+zakariiia@users.noreply.github.com>
2024-01-15 15:44:44 +0100

    Update style for base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class."""
 from datetime import datetime
 import uuid
 import models
 
 
 class BaseModel:
+    """Represent BaseModel of the HBnB project."""
 
     def __init__(self, *arg, **kwarg):
-        """
+        """Initialize a new BaseModel.
 
-
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
@@ -20,25 +25,18 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 if key == 'updated_at' or key == 'created_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else :
+                    setattr(self, key, datetime.strptime(value, tform))
+                else:
                     setattr(self, key, value)
         models.storage.new(self)
 
     def save(self):
-        
-        """
-        
-        
-        """
+        """Update updated_at with the current date time."""
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-
-        """
-        
-        """
+        """Return the dic to basemodel."""
         instan_dict = self.__dict__.copy()
         instan_dict["__class__"] = self.__class__.__name__
         instan_dict["created_at"] = self.created_at.isoformat()
@@ -47,10 +45,7 @@ class BaseModel:
         return instan_dict
 
     def __str__(self):
-            
-        """
-
-        """
+        """Return or print the instance of the base model."""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
     
